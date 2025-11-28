d6ff62fbd423b1cfe0e755718744d114ae900ef5
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2025-11-28 15:30:39 +0000

    tweak blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,15 +1,15 @@
 return {
-	"saghen/blink.cmp",
-	opts = {
-		keymap = {
-			preset = "enter",
-			["<CR>"] = { "fallback" },
-			["<S-CR>"] = { "accept", "fallback" },
-			["<S-Tab>"] = {
-				LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
-				"fallback",
-			},
-			["<Tab>"] = { "select_next", "fallback" },
-		},
-	},
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      preset = "enter",
+      ["<CR>"] = { "fallback" },
+      ["<S-CR>"] = { "accept", "fallback" },
+      ["<S-Tab>"] = {
+        LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
+        "fallback",
+      },
+      ["<Tab>"] = { "fallback" },
+    },
+  },
 }
